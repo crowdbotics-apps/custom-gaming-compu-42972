cc52a3f5faa6bc232f6b2f9bd619a26d6b90aee4
Crowdbotics <team@crowdbotics.com>
2023-08-21 02:12:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "custom-gaming-compu-42972",
+  url: "https://custom-gaming-compu-42972.botics.co",
+  api: "https://custom-gaming-compu-42972.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
